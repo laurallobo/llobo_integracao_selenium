7864597050c7cafde75b3394b9d5b0d3da698ec0
laurallobo <laura.llobo@gmail.com>
2022-10-29 07:04:36 -0300

    primeiro commit

---
+++ src/test/java/br/com/llobo/testacases/TesteClass.java
@@ -0,0 +1,12 @@
+package br.com.llobo.testacases;
+
+import org.testng.annotations.Test;
+
+public class TesteClass {
+	
+	@Test
+	public void teste01() {
+		System.out.println("Teste 01");
+	}
+
+}
